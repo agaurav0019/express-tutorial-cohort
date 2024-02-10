baca8c5b157864105cf18b5923788fa06123ec46
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2024-02-10 17:33:21 +0530

    Basic Routes in express

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
-const express = require("express");
+// Creating in memory hospital where datas are stored in variables and not in database
+// Create 4 routes 
+// GET - users can check how many kidneys they have and their health
+// POST - user can add a new kidney
+// PUT - user can replace a kidney, make it healthy
+// DELETE - user can remove a kidney
 
+const express = require("express");
 const app = express();
 
-function sum(n){
-    let a = 0;
-    for(let i=1;i<=n;i++){
-        a = a+i;
-    }
-    return a;
-}
+const users = [{
+    name:"John",
+    kidneys : [{
+        healthy: false
+    }]
+}];
+
+app.use(express.json());
 
 app.get("/", function(req,res){
-    const n = parseInt(req.query.n); // Parsing the query parameter to an integer
-    const ans = sum(n);
-    res.status(200).send(`Hi, your answer is ${ans}`); // Sending status code and response together
+    const johnKidneys = users[0].kidneys;
+    const noOfKidneys = johnKidneys.length;
+    let noOfHealthyKidneys = 0;
+    for(let i=0;i<noOfKidneys;i++){
+        if(johnKidneys[i].healthy){
+            noOfHealthyKidneys += 1;
+        }
+    }
+    const noOfUnHealthyKidneys = noOfKidneys-noOfHealthyKidneys;
+    res.json({
+        noOfKidneys,
+        noOfHealthyKidneys,
+        noOfUnHealthyKidneys
+    })
+});
+
+app.post("/",function(req,res){
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy:isHealthy
+    })
+    res.json({
+        msg:"Done!"
+    })
+})
+
+app.put("/", function(req,res){
+    for(let i=0;i<users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({msg: "Done!"})
+})
+
+app.delete("/", function(req,res){
+    const newKidney = [];
+    for(let i=0;i<users[0].kidneys.length; i++){
+        newKidney.push({
+            healthy: true
+        })
+    }
+    users[0].kidneys = newKidney;
+    res.json({msg: "Done!"})
 })
 
 app.listen(3000);
